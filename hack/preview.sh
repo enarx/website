9923a04840c52aed35b7c681658d3663967a8799
Paul Pietkiewicz <paul@profian.com>
2022-07-05 16:03:06 -0400

    feat: add menu item for Enarx_toml documentation
    Signed-off-by: Paul Pietkiewicz <paul@profian.com>

--- hack/preview.sh
+++ hack/preview.sh
@@ -8,9 +8,10 @@ echo "REF: ${REF}"
 (test -d versioned && echo "Deleting old versioned directory" && rm -rf versioned)
 git clone --branch ${REF} https://github.com/${ORG}/enarx.git versioned
 
-(test -d versioned/docs/ && rsync -avpL --ignore-times versioned/docs/* versioned_staging) || echo versioned docs do not exist, skipping step
-(test -d versioned_staging && rsync -avpL --ignore-times versioned_staging/* docs/) || echo versioned docs do not exist, skipping step
-(test -d override && rsync -avpL --ignore-times override/* docs/) || echo override docs do not exist, skipping step
+(test -d versioned/docs/ && rsync -avp --no-links --ignore-times versioned/docs/* versioned_staging) || echo versioned docs do not exist, skipping step
+(test -d versioned/crates/enarx-config/ && cp versioned/crates/enarx-config/Enarx_toml.md versioned_staging) || echo enarx_config versioned docs do not exist, skipping step
+(test -d versioned_staging && rsync -avp --no-links --ignore-times versioned_staging/* docs/) || echo versioned docs do not exist, skipping step
+(test -d override && rsync -avp --no-links --ignore-times override/* docs/) || echo override docs do not exist, skipping step
 
 yarn install
 yarn lint
