3b2556ae6c3a6c49484bdd01fff0227ee17b9d05
Paul Pietkiewicz <paul@profian.com>
2022-07-05 12:31:18 -0400

    build: support symlinks
    Signed-off-by: Paul Pietkiewicz <paul@profian.com>

--- hack/preview.sh
+++ hack/preview.sh
@@ -8,9 +8,9 @@ echo "REF: ${REF}"
 (test -d versioned && echo "Deleting old versioned directory" && rm -rf versioned)
 git clone --branch ${REF} https://github.com/${ORG}/enarx.git versioned
 
-(test -d versioned/docs/ && rsync -avp --ignore-times versioned/docs/* versioned_staging) || echo versioned docs do not exist, skipping step
-(test -d versioned_staging && rsync -avp --ignore-times versioned_staging/* docs/) || echo versioned docs do not exist, skipping step
-(test -d override && rsync -avp --ignore-times override/* docs/) || echo override docs do not exist, skipping step
+(test -d versioned/docs/ && rsync -avpL --ignore-times versioned/docs/* versioned_staging) || echo versioned docs do not exist, skipping step
+(test -d versioned_staging && rsync -avpL --ignore-times versioned_staging/* docs/) || echo versioned docs do not exist, skipping step
+(test -d override && rsync -avpL --ignore-times override/* docs/) || echo override docs do not exist, skipping step
 
 yarn install
 yarn lint
