4df97df2508c730a905b961221acd8147dc1b3b9
Paul Pietkiewicz <paul@profian.com>
2022-07-20 17:58:31 -0400

    chore: prepare documentation for 0.6.1 release
    Signed-off-by: Paul Pietkiewicz <paul@profian.com>

--- hack/preview.sh
+++ hack/preview.sh
@@ -9,7 +9,7 @@ echo "REF: ${REF}"
 git clone --branch ${REF} https://github.com/${ORG}/enarx.git versioned
 
 (test -d versioned/docs/ && rsync -avp --no-links --ignore-times versioned/docs/* versioned_staging) || echo versioned docs do not exist, skipping step
-if [ ! -d versioned_staging/Running ];then mkdir versioned_staging/Running; fi
+if [ ! -d versioned_staging/Running ];then mkdir -p versioned_staging/Running; fi
 (test -d versioned/crates/enarx-config/ && cp versioned/crates/enarx-config/Enarx_toml.md versioned_staging/Running/Enarx_toml.md) || echo enarx_config versioned docs do not exist, skipping step
 (test -d versioned_staging && rsync -avp --no-links --ignore-times versioned_staging/* docs/) || echo versioned docs do not exist, skipping step
 (test -d override && rsync -avp --no-links --ignore-times override/* docs/) || echo override docs do not exist, skipping step
