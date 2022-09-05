7ac33de0362a84f8a24f4a7e03a521e792321c01
Nick Vidal <nick@profian.com>
2022-09-05 20:22:29 -0300

    docs: add download/try enarx buttons
    Signed-off-by: Nick Vidal <nick@profian.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Enarx',
-  tagline: 'WebAssembly + Confidential Computing',
+  tagline: 'Confidential Computing with WebAssembly',
   url: 'https://enarx.dev',
   baseUrl: '/',
   onBrokenLinks: 'throw',
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,8 +15,12 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/docs/Start/Introduction">
-            Enarx Introduction - 10min ⏱️
+          <Link className="button button--primary button--lg" to="/docs/Quickstart">
+            Download
+          </Link>
+          <span>&nbsp; &nbsp;</span>
+          <Link className="button button--info button--lg" to="https://try.enarx.dev">
+            Try Enarx
           </Link>
         </div>
       </div>
@@ -27,7 +31,7 @@ function HomepageHeader() {
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout title={`${siteConfig.title}`} description="Enarx: Confidential Computing + WebAssembly">
+    <Layout title={`${siteConfig.title}`} description="Enarx: Confidential Computing with WebAssembly">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
