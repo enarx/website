9ec621828c21891d0ed14e2a15fc3186b322208b
Patrick Uiterwijk <patrick@profian.com>
2023-01-12 18:03:09 +0100

    chore: update to gtag
    Google Analytics Universal has been deprecated, so move to the new system.
    
    Signed-off-by: Patrick Uiterwijk <patrick@profian.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
-        googleAnalytics: {
+        gtag: {
           trackingID: 'G-5D36SLK3FB',
           anonymizeIP: true,
         },
